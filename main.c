1d00aaadebea77c76fb6f787e834dc91ae65bc7a
enesakbal <60822023+enesakbal@users.noreply.github.com>
2021-02-05 13:20:49 +0300

    Problem - 2
    Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be:
    
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.

--- main.c
+++ main.c
@@ -2,18 +2,26 @@
 #include <stdlib.h>
 int main(){
 
-
-int i=1;
+int sayi1=0;
+int sayi2=1;
+int temp;
 int toplam=0;
-
-for (int i=3;i<1000;i++){
-    if (i%3==0 || i%5==0){
-        toplam+=i;
+for (int i=1;i<5000;i++){
+    if (sayi1>4000000){
+        break;
+    }
+    temp=sayi1+sayi2;
+    sayi2=sayi1;
+    sayi1=temp;
+    printf("%d ",sayi1);
+    if (sayi1%2==0){
+      toplam+=sayi1;
     }
 
 }
+printf("\nToplam : %d",toplam);
 
-printf("%d",toplam);
+
 
 
 
